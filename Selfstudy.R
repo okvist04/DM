6f28797485ccfda171b0d6110575bead1a10def3
Louiseneema <lnkp18@student.aau.dk>
2021-04-15 11:31:07 +0200

    nvkjdf

--- Selfstudy.R
+++ Selfstudy.R
@@ -5,4 +5,7 @@ covid_data <- read.table("Data_COVID_OWID_per_capita.csv", header = TRUE, sep = 
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 =======
+  
+  
+  
 >>>>>>> 11fd1a48a69002296afb49b1a2ef4a14d48295e9
