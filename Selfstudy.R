5286c6784170fb7c62dfcf6cb209fcd0a4f26230
Olivia Kvist <okvist18@student.aau.dk>
2021-04-15 10:51:19 +0200

    First

---
+++ Selfstudy.R
@@ -0,0 +1,5 @@
+covid_data <- read.table("Data_COVID_OWID_per_capita.csv", header = TRUE, sep = ",", 
+                         stringsAsFactors = FALSE)
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
