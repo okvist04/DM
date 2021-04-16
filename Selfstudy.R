bcb50a8ff87e5e89084a330048d82de8b632acd0
Olivia Kvist <okvist18@student.aau.dk>
2021-04-16 09:26:34 +0200

    Final

--- Selfstudy.R
+++ Selfstudy.R
@@ -2,14 +2,27 @@ covid_data <- read.table("Data_COVID_OWID_per_capita.csv", header = TRUE, sep = 
                          stringsAsFactors = FALSE)
 
 my.data <- data.frame(scale(covid_data[,-1])) #Data without countries
-data1 <- na.omit(my.data) #Data where every row with NaN's are removed. 
+data_noNA <- na.omit(covid_data) #Data where every row with NaN's are removed. 
+boxplot(data_noNA)
+
+data.pca <- prcomp(data_noNA[,c(1:28,30,45)], center = TRUE, scale. = TRUE)
+
+summary(data.pca)
+str(data.pca)
+
+library(devtools)
+install_github("vqv/ggbiplot")
+library(ggbiplot)
+
+ggbiplot::ggbiplot(data.pca, labels = rownames(data_noNA), xlim = c(-10,10), ylim = c(-4,4))
+
+
 set.seed(1)
 #Randomly take 13 observations out of the 27 total observations
 train <- sample(27, 17)
 
 model <- lm(TotalDeaths ~ . -TotalCases, data = data1, subset = train)
 mean((data1$TotalDeaths - predict(model, data1))[-train]^2)
-
-
+summary(model)
 
 
